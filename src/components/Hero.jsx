f5a3dc8b2d66d54e85ad8d126e0987bc056c8e93
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-08 09:04:49 +0545

    fixed directories

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+const HeroSection=() =>{
+    return(
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE
+                     TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE
+                     THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+                <div className="hero-btn">
+                    <button>SHOW NOW</button>
+                    <button>Category</button>
+
+
+
+                </div>
+                <div className="shopping">
+                    <p>Also Available On</p>
+                </div>
+
+
+            </div>
+        </main>
+    );
+};
+export default HeroSection;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+
+const Navigation = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="" alt="Logo" />
+            </div>
+            <ul>
+              <li href="#">Menu</li>
+              <li href="#">Contact</li>
+              <li href="#">About</li>
+              <li href="#">Address</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    );
+};
+
+export default Navigation;
